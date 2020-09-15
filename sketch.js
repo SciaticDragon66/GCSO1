761861299c02a7314bbc6bd72e7d45404f0476b3
SciaticDragon66 <70347881+SciaticDragon66@users.noreply.github.com>
2020-09-15 16:27:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
+var car,wall
+var speed,height
+
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
+  speed = random(55,90);
+  weight = random (400,1500);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255); 
+  car = createSprite (50, 200, 50, 50);
+  wall = createSprite (1500, 200, 60, height/2);
+  car.velocityX = speed;
+  car.collide = wall; 
   drawSprites();
 }
